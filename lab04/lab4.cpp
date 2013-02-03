48db4494e8e165172d3bfd0290ca071c18eed426
Yu An Li <yli015@usc.edu>
2013-02-03 14:23:28 -0800

    Part 1

--- lab04/lab4.cpp
+++ lab04/lab4.cpp
@@ -4,63 +4,103 @@
 
 using namespace std;
 
-class List
+template <class T>
+class MyList
 {
 	private:
 		int size_;
 		int max_size_;
-		string* names_;
+		T* names_;
 	public:
-		List();
-		~List();
-		void push_back(string name);
+		MyList();
+		~MyList();
+		void push_back(T name);
 		int size() const;
-		string at(int loc) const;		
+		T at(int loc) const;		
 };
 
-List::List()
+template <class T>
+MyList<T>::MyList()
 {
 	max_size_ = 5;
 	size_ = 0;
-	names_ = new string[max_size_];
+	names_ = new T[max_size_];
 }
 
-List::~List()
+template <class T>
+MyList<T>::~MyList()
 {
-	delete names_;
 }
 
-void List::push_back(string name)
+template <class T>
+void MyList<T>::push_back(T name)
 {
 	if (size_ == max_size_)
 	{
-		string* new_name_array = new string[max_size_*=2];
+		T* new_name_array = new T[max_size_*=2];
 		for (int i = 0; i < size_; i++)
 		{
 			new_name_array[i] = names_[i];
 		}
 		delete [] names_;
 		names_ = new_name_array;
-	}
+	}   
 	names_[size_] = name;
 	size_++;
 }
 
-int List::size() const
+template <class T>
+int MyList<T>::size() const
 {
 	return size_;
 }
 
-string List::at(int loc) const
+template <class T>
+T MyList<T>::at(int loc) const
 {
-	if(loc >= size_ || loc<0 )
-	{
-		return "failed";
-	}
 	return names_[loc];
 }
 
 int main()
 {
+	cout << "begining program" << endl;
+	
+	// create int and string lists
+	MyList <int> int_list;	
+	MyList <string> string_list;
+	
+	cout << "Adding values..." << endl;
+	
+	//pushback a few integers and strings
+	int_list.push_back(0);
+	int_list.push_back(1);
+	int_list.push_back(2);
+	string_list.push_back("hello ");
+	string_list.push_back("world ");
+	string_list.push_back("again ");
+	string_list.push_back("and ");
+	string_list.push_back("again ");
+	string_list.push_back("and ");
+	string_list.push_back("again.");
+	
+	cout << "Size of integer list: " << int_list.size() << endl;
+	cout << "Size of string list: " << string_list.size() << endl << endl;
+	
+	//iterate and disply values
+	cout << "Printing Integer List: ";
+	for (int i = 0; i < int_list.size(); i++)
+	{
+		cout << int_list.at(i);
+	}
+	cout << endl << endl;
+	
+	cout << "Printing String List: ";
+	for (int i = 0; i < string_list.size(); i++)
+	{
+		cout << string_list.at(i);
+	}
+	cout << endl ;
+	
+	cout << "done" << endl;
 	return 0;
 }
